dda2a19c8a72d189030bfb01b35ed2ebd16530e3
Samara Silva <samarasilva.dev@gmail.com>
2022-06-17 20:08:50 -0300

    Atualizando um registro na tabela aluno

--- tabela_aluno.sql
+++ tabela_aluno.sql
@@ -17,3 +17,14 @@ SELECT * FROM aluno;
 INSERT INTO aluno (nome, cpf, observacao, idade, dinheiro, altura, ativo, data_nascimento, hora_aula, matriculado_em)
 VALUES ('Maria', '12345678901', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget erat sed nulla commodo varius quis suscipit ex. Nunc facilisis metus sapien, ac pulvinar nisl facilisis ut. Cras vestibulum euismod feugiat. Mauris viverra nulla ut sem imperdiet, vel condimentum felis faucibus. Nulla facilisis erat quam. Ut eget interdum eros. Vestibulum pretium lacus vel ante commodo, ut efficitur sapien fringilla. Vestibulum interdum tristique justo in feugiat. Ut a imperdiet diam. Morbi finibus ultrices tortor, ac posuere nunc blandit at. Nunc at libero justo.
 ', 32, 5000.00, 1.70, TRUE, '1990-05-24', '19:38:00', '2022-06-17 19:38:00');
+
+UPDATE aluno 
+SET  nome = 'Mariana', 
+	 cpf = '11122233344', 
+	 observacao = 'Teste',
+	 dinheiro = 8000.00, 
+	 altura = 1.68, 
+	 ativo = FALSE, 
+	 data_nascimento = '1994-06-20', 
+	 hora_aula = '19:00:00', 
+	 matriculado_em = '2022-06-16 16:30:00'
