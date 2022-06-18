442e3aea92296dbb60c6702e7340b9f5b26abb28
Samara Silva <samarasilva.dev@gmail.com>
2022-06-18 20:53:03 -0300

    Inserção de dados na tabela curso

--- tabela_curso.sql
+++ tabela_curso.sql
@@ -2,3 +2,6 @@ CREATE TABLE curso(
 	id INTEGER PRIMARY KEY,
 	nome VARCHAR(200) NOT NULL
 );
+
+INSERT INTO curso (id, nome) VALUES (1, 'HTML');
+INSERT INTO curso (id, nome) VALUES (2, 'javascript')
