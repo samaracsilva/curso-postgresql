61660318de5fe1ba9f9a65e41ad6b185091b09a7
Samara Silva <samarasilva.dev@gmail.com>
2022-06-18 01:12:43 -0300

    Ulitizando mais de OR no filtro

--- filtro_operadores_logicos.sql
+++ filtro_operadores_logicos.sql
@@ -3,3 +3,6 @@ WHERE nome LIKE 'M%' AND cpf IS NOT Null
 
 SELECT * FROM aluno 
 WHERE nome LIKE 'Maria' OR nome LIKE 'Ana'
+
+SELECT * FROM aluno 
+WHERE nome LIKE 'Maria' OR nome LIKE 'Ana' OR nome LIKE 'Nico'
