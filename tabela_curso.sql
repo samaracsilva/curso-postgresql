2ad750326c4f880518de7445fa8a13af465d5ae2
Samara Silva <samarasilva.dev@gmail.com>
2022-06-18 23:09:09 -0300

    Criando novas tabelas para criar relacionamentos

--- tabela_curso.sql
+++ tabela_curso.sql
@@ -5,3 +5,25 @@ CREATE TABLE curso(
 
 INSERT INTO curso (id, nome) VALUES (1, 'HTML');
 INSERT INTO curso (id, nome) VALUES (2, 'javascript')
+
+CREATE TABLE aluno (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR (200) NOT NULL
+);
+
+INSERT INTO aluno (nome) VALUES ('Diogo');
+INSERT INTO aluno (nome) VALUES ('Vinicius');
+
+CREATE TABLE aluno_curso (
+	aluno_id INTEGER,
+	curso_id INTEGER,
+	PRIMARY KEY (aluno_id, curso_id),
+	
+	FOREIGN KEY (aluno_id)
+		REFERENCES aluno (id),
+	FOREIGN KEY (curso_id)
+		REFERENCES curso (id)
+);
+
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (1,1);
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,1);
